df5f568a615dbd189948259c7ea2b189a7d174f1
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-05-29 21:07:50 -0700

    Change the config name to match the extension point name.

--- pulumi-orca/src/main/kotlin/io/armory/plugin/stage/pulumi/PulumiConfig.kt
+++ pulumi-orca/src/main/kotlin/io/armory/plugin/stage/pulumi/PulumiConfig.kt
@@ -1,6 +1,7 @@
 package io.armory.plugin.stage.pulumi
 
 import com.netflix.spinnaker.kork.plugins.api.ExtensionConfiguration
+import com.netflix.spinnaker.kork.plugins.api.PluginConfiguration
 import io.armory.plugin.stage.pulumi.model.Account
 import io.armory.plugin.stage.pulumi.model.Credentials
 
@@ -9,7 +10,7 @@ import io.armory.plugin.stage.pulumi.model.Credentials
  * The data can be key/value pairs or an entire configuration tree.
  *
  */
-@ExtensionConfiguration("pulumi")
+@ExtensionConfiguration("armory.pulumiStage")
 data class PulumiConfig(
         var credentials: Credentials,
         var accounts: Account
